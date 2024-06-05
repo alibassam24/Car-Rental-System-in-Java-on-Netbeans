2db468e266c271dc4cb2b7a18657c95f4405496b
Ali Bassam <145041329+alibassam24@users.noreply.github.com>
2024-06-05 23:46:01 +0500

    Add files via upload

---
+++ car rental sytem/Database + Ucanaccess Libraries/CRMS.sql
@@ -0,0 +1,14 @@
+Select * from Admin
+Select * from Customer
+Select * from Employee
+Select * from Receipt
+Select * from RentACar
+Select * from Vehicle
+Select * from Review
+
+
+SELECT name as 'Procedures' FROM sys.procedures
+SELECT name as 'Views' FROM sys.views
+SELECT name as 'Triggers' FROM sys.triggers
+
+exec insert
